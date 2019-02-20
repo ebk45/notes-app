c31348e875463be215427b27a744d97c09144083
ellie turnock <ebturnock@gmail.com>
2019-02-20 14:30:49 +0000

    refactored test for Note List View

--- notes-tests.js
+++ notes-tests.js
@@ -1,18 +1,19 @@
 'use-strict';
 
+
 function testNoteTextContainsString() {
-  var note = new Note("Javascript is my favourite language");
+  let note = new Note("Javascript is my favourite language");
   assert.isTrue(note.getNote() === "Javascript is my favourite language");
-};
+}
 
 function testNoteListContainsNote() {
-  var noteList = new NoteList();
+  let noteList = new NoteList();
   noteList.createNote("going shopping");
   assert.isTrue(noteList.showList()[0].getNote().includes("going shopping"))
-};
+}
 
 function testNoteListViewShowsNotes() {
-  var noteListView = new NoteListView(new NoteList);
+  let noteListView = new NoteListView(new NoteList);
   noteListView.addNote("Favourite Drink: Vodka");
   noteListView.addNote("Favourite Food: Vodka");
   assert.isTrue(noteListView.viewList() === "<ul><li><div>Favourite Drink: Vodka</div></li><li><div>Favourite Food: Vodka</div></li></ul>")
