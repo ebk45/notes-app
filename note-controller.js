594eeb71ad69aacc7ae930a18b4213c6d59066f6
ellie turnock <ebturnock@gmail.com>
2019-02-21 11:12:42 +0000

    note controller can add note to note list

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,15 @@
+
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+  }
+
+  NoteController.prototype.addNote = function(note) {
+    this.noteList.createNote(note);
+  };
+
+})(this);
+
 var elem = document.getElementById('app');
 console.log(elem)
 
